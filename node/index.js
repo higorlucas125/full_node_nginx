14b829e8654a7000b52077e093839fedb6f1a888
higorlucas125 <higorlaf@hotmail.com>
2023-01-11 12:45:56 -0300

    fix index.js node

--- node/index.js
+++ node/index.js
@@ -4,16 +4,16 @@ const startDb  = require('./database.js');
 const app = express();
 const port = 3000;
 
-startDb.createAndInsertMysql(mysql);
 app.use(express.json());
 app.get("/",(req,res) => {
     res.send("<h1>Full Cycle Rocks!</h1>")
 });
 
 app.get("/list",(req,res) => {
+    startDb.createAndInsertMysql(mysql);
     const query = `SELECT * FROM people`
     startDb.connectNew(query,res,mysql);
-})
+});
 
 app.listen(port,() =>{
     console.log(`Serviço está sendo executado com sucesso na porta ${port}`)
